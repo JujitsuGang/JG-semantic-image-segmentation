59b5c1f803c32dc524a5b42f010aa57a651d5e85
JujitsuGang <105345817+JujitsuGang@users.noreply.github.com>
2022-11-05 09:47:43 +0000

    release notes

--- grpc_client.py
+++ grpc_client.py
@@ -11,4 +11,9 @@ from services.snet import snet_setup
 from services import registry
 
 import services.service_spec.segmentation_pb2_grpc as grpc_bt_grpc
-import service
+import services.service_spec.segmentation_pb2 as grpc_bt_pb2
+
+SERVER_NAME = 'mask_rcnn_server'
+
+
+def save_img(fn, 
